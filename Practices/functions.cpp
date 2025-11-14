55a33e7508e9f87b7575f09fdc1f0c4b8115c3c7
Muhammad Hafiz Suhaili <232006951+ExoticCrafter@users.noreply.github.com>
2025-11-14 10:43:41 +0800

    Update functions.cpp

--- Practices/functions.cpp
+++ Practices/functions.cpp
@@ -5,6 +5,17 @@ using namespace std;
 
 int main(){
   double area, radius;
+  int num1, num2;
+  cout << "What is the radius?\t";
+  cin >> radius
+  area = 3.142 * pow(radius,2.0);
+  cout << "The area is " << area << endl;
+
+  cout << "Enter two numbers to square root" <<endl;
+  cin >> num1, num2;
+  float sqrtAns = sqrt(num1 + num2);
+  cout << "The square root of " << num1+num2 << "is " << sqrtAns <<endl;
+
   
   return 0;
 }
