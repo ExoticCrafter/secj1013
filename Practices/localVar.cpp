cee0ce0d88eb79ad40b18711ca7b8ce522614643
Muhammad Hafiz Suhaili <232006951+ExoticCrafter@users.noreply.github.com>
2025-12-05 10:46:42 +0800

    Create localVar.cpp

---
+++ Practices/localVar.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+void showVar();
+
+int main(){
+    for (int count = 0; count <10; count++){
+        showVar();
+    }
+    system("pause");
+    return 0;
+}
+
+void showVar(){
+    static int var = 10;
+    cout << var << endl;
+    var++;
+}
